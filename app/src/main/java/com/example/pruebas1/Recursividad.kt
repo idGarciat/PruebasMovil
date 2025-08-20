e71d0b7d71b6cc1e2d22adb6b2ec7a00fb6b7fe3
Ismael Derick Garcia Torricos <ismaelgarci_88@hotmail.com>
2025-08-19 23:04:01 -0400

    Procedimientos

--- app/src/main/java/com/example/pruebas1/Recursividad.kt
+++ app/src/main/java/com/example/pruebas1/Recursividad.kt
@@ -4,51 +4,86 @@ import android.widget.TextView
 
 class Recursividad {
 
-
-    fun factorial(a: Int): Int {
-        if(a == 0) {
-            return 1
-        } else{
-            return a * factorial(a - 1)
-        }
-    }
+//    fun factorial(a: Int): Int {
+//        if(a == 0) {
+//            return 1
+//        } else{
+//            return a * factorial(a - 1)
+//        }
+//    }
 
     fun factorialConProceso(n: Int, pasos: MutableList<String>): Int {
         return if (n == 0) {
-            pasos.add("0! = 1  (caso base)")
+            pasos.add("0! = 1")
             1
         } else {
             pasos.add("$n! = $n * (${n-1})!")
             val resultado = n * factorialConProceso(n - 1, pasos)
-            pasos.add("Resultado parcial de $n! = $resultado")
+            pasos.add("$n! = $resultado")
+            resultado
+        }
+    }
+
+//    fun potencia(a: Int, b: Int): Int {
+//
+//        if(b == 0) {
+//            return 1
+//        } else{
+//            return a * potencia(a, b - 1)
+//        }
+//    }
+
+    fun potenciaConProceso(a: Int, b: Int, pasos: MutableList<String>): Int {
+        return if (b == 0) {
+            pasos.add("${a}^0 = 1")
+            1
+        } else {
+            pasos.add("${a}^$b = ${a} * ${a}^${b - 1}")
+            val resultado = a * potenciaConProceso(a, b - 1, pasos)
+            pasos.add("${a}^$b = $resultado")
+            resultado
+        }
+    }
+
+//    fun multiRecursiva(a: Int, b: Int): Int {
+//        if(b == 0) {
+//            return 0
+//        } else {
+//
+//            return a + (multiRecursiva(a, b - 1))
+//        }
+//    }
+
+    fun multiplicacionConProceso(a: Int, b: Int, pasos: MutableList<String>): Int {
+        return if (b == 0) {
+            pasos.add("${a} * 0 = 0")
+            0
+        } else {
+            pasos.add("${a} * $b = $a + (${a} * ${b - 1})")
+            val resultado = a + multiplicacionConProceso(a, b - 1, pasos)
+            pasos.add("${a} * $b = $resultado")
             resultado
         }
     }
 
 
-    fun potencia(a: Int, b: Int): Int {
+//    fun sumaRecursiva(a: Int, b: Int): Int {
+//        if(b == 0) {
+//            return a
+//        } else{
+//            return sumaRecursiva(a+1, b - 1)
+//        }
+//    }
 
-        if(b == 0) {
-            return 1
-        } else{
-            return a * potencia(a, b - 1)
-        }
-    }
-
-    fun multiRecursiva(a: Int, b: Int): Int {
-        if(b == 0) {
-            return 0
+    fun sumaConProceso(a: Int, b: Int, pasos: MutableList<String>): Int {
+        return if (b == 0) {
+            pasos.add("$a + 0 = $a")
+            a
         } else {
-
-            return a + (multiRecursiva(a, b - 1))
-        }
-    }
-
-    fun sumaRecursiva(a: Int, b: Int): Int {
-        if(b == 0) {
-            return a
-        } else{
-            return sumaRecursiva(a+1, b - 1)
+            pasos.add("$a + $b = (${a}+1) + (${b-1})")
+            val resultado = sumaConProceso(a + 1, b - 1, pasos)
+            pasos.add("${a}+${b} = $resultado")
+            resultado
         }
     }
 
